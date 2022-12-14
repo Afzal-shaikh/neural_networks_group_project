40b3e4f721ce92f2ca59dfb05690c3d62fe231f3
Manvir Kaur <mkaur791@my.centennialcollege.ca>
2022-12-13 23:25:34 -0500

    added frontend

--- API/flask_neural_network.py
+++ API/flask_neural_network.py
@@ -4,6 +4,7 @@ from flask import Flask, request, jsonify
 import traceback
 import tensorflow as tf
 from flask_cors import CORS
+import numpy as np
 
 
 # Your API definition
@@ -15,11 +16,11 @@ def predict():
     if loaded_model:
         try:
             json_ = request.json
-            print('JSON: \n', json_)
+            print('JSON: \n', json_)           
             query = pd.DataFrame(json_, columns=model_columns)
             prediction = list(loaded_model.predict(query))
             print('prediction=', prediction)
-            res = jsonify({"prediction": str(prediction)})
+            res = jsonify({"prediction": str(prediction[0])})
             res.headers.add('Access-Control-Allow-Origin', '*')
             return res
         except:
@@ -36,7 +37,7 @@ if __name__ == '__main__':
         port = 12345 # If you don't provide any port the port will be set to 12345
         
     # load  model:
-    loaded_model = tf.keras.models.load_model('tensorflow_model')
+    loaded_model = tf.keras.models.load_model('../training_neural_network/tensorflow_model')
     print(f'Model {loaded_model} loaded')
         
     model_columns =["First Term Gpa", "Second Term Gpa", "First Language", "Funding numeric", "School numeric", "FastTrack numeric",
